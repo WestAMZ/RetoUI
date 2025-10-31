6679a18608e66036412b65b211e9e2db39f00f51
Westly Meza <westlymeza@gmail.com>
2025-10-31 16:52:51 -0600

    Adding reader

---
+++ RetoUI/Data/DataReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RetoUI
+{
+    internal class DataReader
+    {
+        public static Root Read()
+        {
+            Root root = null;
+            try
+            {
+                // Ruta del archivo JSON
+                //string filePath = Application.StartupPath + ".\\...\\...\\data.json";
+                string filePath = "\\...\\...\\database.json";
+
+                // Leer todo el contenido del archivo
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deserializar el JSON en el objeto Root
+                root = JsonSerializer.Deserialize<Root>(jsonContent);
+
+                // Acceder a los datos
+                Console.WriteLine($"Usuario: {root.settings.username}");
+                Console.WriteLine("Clientes:");
+
+                foreach (var c in root.data.customers)
+                {
+                    Console.WriteLine($"ID: {c.id}");
+                    Console.WriteLine($"Nombre: {c.name}");
+                    Console.WriteLine($"Teléfono: {c.cellphone}");
+                    Console.WriteLine($"Dirección: {c.address}");
+                    Console.WriteLine("---------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al leer o deserializar el archivo: {ex.Message}");
+            }
+            return root;
+        }
+    }
+}
---
+++ RetoUI/DataDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace RetoUI
+{
+    public class Root
+    {
+        public Data data { get; set; }
+        public Settings settings { get; set; }
+    }
+
+    public class Data
+    {
+        public List<Customer> customers { get; set; }
+        public List<Sale> sales { get; set; }
+    }
+
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string cellphone { get; set; }
+        public string address { get; set; }
+    }
+
+    public class Sale
+    {
+        // Puedes definir las propiedades de las ventas aquí
+    }
+
+    public class Settings
+    {
+        public string username { get; set; }
+    }
+
+}
+
+
